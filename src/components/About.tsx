e0d706c2b4ace0899e02944e9b892213c2787ab4
Harry Jenkins <harry@and-element.com>
2025-06-29 23:59:50 +0100

    fix about spacing

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,28 +9,31 @@ export default function About() {
 	return (
 		<div>
 			<h2 className="text-primary my-6">About_Me/</h2>
-			<p>
-				My name is <mark>{data.name}</mark>. I am a <mark>{data.jobTitle}</mark> with{' '}
-				<mark>{expYears} years of experience</mark>. I discovered my passion for programming
-				at the age of 14 when I started programming with Lua. Since then, I have constantly
-				been improving my skills and learning new technologies. My strong interest in{' '}
-				<mark>security</mark> and my value of <mark>privacy</mark> led me to study{' '}
-				<mark>BSc Computer and Cyber Security</mark>, graduating with a{' '}
-				<mark>first class with honours</mark>.
-			</p>
+			<div className="flex flex-col gap-y-6">
+				<p>
+					My name is <mark>{data.name}</mark>. I am a <mark>{data.jobTitle}</mark> with{' '}
+					<mark>{expYears} years of experience</mark>. I discovered my passion for
+					programming at the age of 14 when I started programming with Lua. Since then, I
+					have constantly been improving my skills and learning new technologies. My
+					strong interest in <mark>security</mark> and my value of <mark>privacy</mark>{' '}
+					led me to study <mark>BSc Computer and Cyber Security</mark>, graduating with a{' '}
+					<mark>first class with honours</mark>.
+				</p>
 
-			<p>
-				I have extensive experience working in <mark>Linux-based environments</mark>, and it
-				has been my operating system of choice for <mark>over {linuxYears} years</mark>. I
-				use <mark>Linux</mark> daily, both professionally and personally, and I am highly
-				proficient in its ecosystem.
-			</p>
+				<p>
+					I have extensive experience working in <mark>Linux-based environments</mark>,
+					and it has been my operating system of choice for{' '}
+					<mark>over {linuxYears} years</mark>. I use <mark>Linux</mark> daily, both
+					professionally and personally, and I am highly proficient in its ecosystem.
+				</p>
 
-			<p>
-				In my free time, I enjoy watching YouTube videos and movies, and 3D printing various
-				projects, which allows me to combine creativity with technology. I am also studying{' '}
-				<mark>Bahasa Indonesia</mark> and have a basic understanding of it.
-			</p>
+				<p>
+					In my free time, I enjoy watching YouTube videos and movies, and 3D printing
+					various projects, which allows me to combine creativity with technology. I am
+					also studying <mark>Bahasa Indonesia</mark> and have a basic understanding of
+					it.
+				</p>
+			</div>
 		</div>
 	);
 }
