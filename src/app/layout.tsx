72b3f270e510e2b92d452e9ea8c10699f94525ba
Harry Jenkins <harry@and-element.com>
2025-07-10 23:14:31 +0100

    update metadata description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,26 @@
-import type { Metadata } from 'next';
 import { JetBrains_Mono } from 'next/font/google';
 import './globals.css';
 
 // components
 import Navbar from '@/components/Navbar';
 import Providers from './providers';
-import { getData, getExperienceYears } from '@/data/data';
+import { getData } from '@/data/data';
 
 const fontMono = JetBrains_Mono({
 	subsets: ['latin'],
 });
 
-const data = getData();
-const experienceYears = getExperienceYears(data.careerStart);
+export async function generateMetadata() {
+	const data = getData();
 
-export const metadata: Metadata = {
-	title: data.name,
-	description: `Hello, I am a ${data.jobTitle} with ${experienceYears} years of experience`,
-	icons: {
-		icon: '/favicon.svg',
-	},
-};
+	return {
+		title: data.name,
+		description: `Hello, I am a ${data.jobTitle}. Passionate about web development, Linux, and security.`,
+		icons: {
+			icon: '/favicon.svg',
+		},
+	};
+}
 
 export default function RootLayout({
 	children,
